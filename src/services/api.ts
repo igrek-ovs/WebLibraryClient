4c932b8473d95cd27468152f39a9e4a3e0602048
igrek-ovs <igrekvovcyn@gmail.com>
2024-02-21 15:28:44 +0200

    code cleanup

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: `${process.env.REACT_APP_API}`,
+    baseURL: `https://library-api-igrek.azurewebsites.net`,
     headers: {
         'Content-Type': 'application/json',
     },
