10c3e84829537dd8dd4fd436f74433d46d2e775d
igrek-ovs <igrekvovcyn@gmail.com>
2024-02-21 14:59:14 +0200

    code cleanup

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API,
+    baseURL: `${process.env.REACT_APP_API}`,
     headers: {
         'Content-Type': 'application/json',
     },
